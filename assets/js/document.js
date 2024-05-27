e2decd7a296e39533638d96ed6d238a0c910fac3
YashpalGawali <gawaliyashpal@gmail.com>
2024-05-27 17:16:45 +0530

    27 May fir commit

--- assets/js/document.js
+++ assets/js/document.js
@@ -45,69 +45,205 @@ $(document).ready(function(){
 			let cur_date  = d.getDate();
 			let cur_year  = d.getFullYear();
 		    
-// 	    	if(cur_month < 10) {
-// 				cur_month = "0"+cur_month;
-// 			}
-
-// 			if(cur_date < 10) {
-// 				cur_date = "0"+cur_date;
-// 			}
 			let cur_total_days = daysInMonth(cur_month, cur_year);
-			let next_total_days = daysInMonth((cur_month+1), cur_year);
-		    let diff_days = cur_total_days - next_total_days;
+			
+			let next_total_days ;
+			let next_month ;
+			let next_year ;
+			let next_date ;
+			
+			if(cur_month==12){
+			    next_month=1;
+                next_total_days = daysInMonth((next_month), (cur_year+1));
+			 }
+			 else
+			 {
+			    next_total_days =  daysInMonth((cur_month+1), cur_year);
+			 }
     
-            alert('Total Days in Selected Month'+cur_total_days);
-            
-            alert('Total Days in Next Month'+next_total_days);
-            alert('Difference in days is '+diff_days);
-            
-            if(cur_year%400===0 || cur_year%4===0)
+            if(cur_total_days==next_total_days)
             {
-                if(cur_month=='01' )
+                alert('curent days and Next month days are EQUAL');
+                if(cur_month<10)
                 {
-                    if(cur_date>=29 && cur_date <=31)
-                    {
-                        let next_month_date = (cur_year)+"-0"+((cur_month)+1)+"-"+cur_date;
-                        alert('Next Month date is '+next_month_date);
-				        $('#last_renewed_date').val(next_month_date);
-                    }
-                }    
+                    cur_month = "0"+cur_month;
+                }
+                if(cur_date<10)
+                {
+                    cur_date = "0"+cur_date;
+                }
+                let next_month_date = (cur_year)+"-"+(cur_month)+"-"+cur_date;
+                $('#last_renewed_date').val(next_month_date);
             }
-            else 
+            
+            if(cur_total_days < next_total_days)
             {
-                if(cur_month=='01' )
+                alert('curent days are less\n Current month is '+cur_month);
+                cur_month+=1;
+                alert('Next month is '+cur_month);
+                if(cur_month<10)
                 {
-                    if(cur_date==29 )
-                    {
-                        let next_month_date = (cur_year)+"-0"+((cur_month)+2)+"-0"+1;
-                        alert('Next Month date is '+next_month_date);
-				        $('#last_renewed_date').val(next_month_date);
-                    }
-                    else if(cur_date==30 )
-                    {
-                        let next_month_date = (cur_year)+"-0"+((cur_month)+2)+"-0"+2;
-                        alert('Next Month date is '+next_month_date);
-				        $('#last_renewed_date').val(next_month_date);
-                    }
-                    else if(cur_date==31 )
-                    {
-                        let next_month_date = (cur_year)+"-0"+((cur_month)+2)+"-0"+3;
-                        alert('Next Month date is '+next_month_date);
-				        $('#last_renewed_date').val(next_month_date);
-                    }
-        			else {
-            			if(cur_date < 10) {
-            				cur_date = "0"+cur_date;
-            			}
-                        let next_month_date = (cur_year)+"-0"+((cur_month)+1)+"-"+cur_date;
-                        alert('Next Month date is '+next_month_date);
-    			        $('#last_renewed_date').val(next_month_date);
-        			}
-                }  
+                    cur_month = "0"+cur_month;
+                }
+                if(cur_date<10)
+                {
+                    cur_date = "0"+cur_date;
+                }
+                let next_month_date = (cur_year)+"-"+(cur_month)+"-"+cur_date;
+                $('#last_renewed_date').val(next_month_date);
             }
+            
+            if(cur_total_days > next_total_days)
+            {
+                alert('curent days are Greater \n Current month is '+cur_month);
+                
+                if(cur_date<=next_total_days)
+                {
+                    cur_month+=1;
+                    if(cur_month<10) {
+                        cur_month = "0"+cur_month;
+                    }
+                    if(cur_date<10) {
+                        cur_date = "0"+cur_date;
+                    }
+                    let next_month_date = (cur_year)+"-"+(cur_month)+"-"+cur_date;
+                    $('#last_renewed_date').val(next_month_date);
+                }
+                if(cur_date>next_total_days)
+                {
+                    let next_date =cur_date-next_total_days;
+                    
+                    if(cur_month ==1) {
+                        cur_month+=2;
+                    }
+                    else {
+                        cur_month+=1;
+                    }
+                    if(cur_month<10) {
+                        cur_month = "0"+cur_month;
+                    }
+                    if(next_date<10) {
+                        next_date = "0"+next_date;
+                    }
+                    let next_month_date = (cur_year)+"-"+(cur_month)+"-"+next_date;
+                    $('#last_renewed_date').val(next_month_date);
+                } 
+                
+            }
+            // alert('Total Days in Selected Month'+cur_total_days);
+            
+            // alert('Total Days in Next Month'+next_total_days);
+            // alert('Difference in days is '+diff_days); 
+            
+            // if(cur_year%400===0 || cur_year%4===0)
+            // {
+            //     if(cur_month=='01' )
+            //     {
+            //         if(cur_date==29 )
+            //         {
+            //             cur_month= cur_month+1;
+            //             if(cur_month<10) {
+            //                 cur_month = "0"+cur_month;
+            //             }
+            //             let next_month_date = (cur_year)+"-"+(cur_month)+"-"+cur_date;
+            //             $('#last_renewed_date').val(next_month_date);
+            //         }
+            //         if(cur_date>=30 && cur_date <=31){
+            //             let daysinnextmonth = daysInMonth((cur_month+1),cur_year);
+            //             let next_date =cur_date-daysinnextmonth;
+            //             if(next_date<10) {
+            //                 next_date = "0"+next_date;
+            //             }
+            //             cur_month=cur_month+2;
+            //             if(cur_month<10) {
+            //                 cur_month = "0"+cur_month;
+            //             }
+                        
+            //             let next_month_date = (cur_year)+"-"+((cur_month))+"-"+next_date;
+            //             $('#last_renewed_date').val(next_month_date);
+            //         }
+            //     }    
+            // }
+            // else 
+            // {
+            //     let next_date = cur_date-next_total_days;
+            //     if(next_date===0){
+            //         next_date = cur_date;
+            //     }
+               
+            //     if(next_total_days>=29 && next_total_days<=31)
+            //     {
+            //         if(next_total_days<cur_total_days){
+                        
+            //             cur_month= cur_month+2;
+            //         }
+            //         else {
+            //             if(cur_month==12)
+            //                 cur_month=1;
+            //             else
+            //                 cur_month= cur_month+1;
+            //         }
+            //         if(cur_month <10) {
+            //             cur_month= "0"+cur_month;
+            //         }
+            //         if(next_date<10) {
+            //             next_date = "0"+next_date;
+            //         }
+            //         if(cur_month==12)
+            //         {
+            //             cur_year+=1;
+            //         }
+            //         let next_month_date = (cur_year)+"-"+((cur_month))+"-"+next_date;
+            //         alert('Next Month date is '+next_month_date);
+            //     }
+            //      else {
+            //         cur_month= cur_month+1;
+            //         if(cur_month <10) {
+            //             cur_month= "0"+cur_month;
+            //         }
+            //         if(cur_date<10) {
+            //             cur_date = "0"+cur_date;
+            //         }
+            //          let next_month_date = (cur_year)+"-"+((cur_month))+"-"+cur_date;
+            //         alert('Next Month date is '+next_month_date);
+            //      }
+			    
+                // if(cur_month=='01' )
+                // {
+        //             let next_month_date = (cur_year)+"-0"+((cur_month)+2)+"-0"+1;
+        //             alert('Next Month date is '+next_month_date);
+			     //   $('#last_renewed_date').val(next_month_date);
+            //         if(cur_date==29 )
+            //         {
+            //             let next_month_date = (cur_year)+"-0"+((cur_month)+2)+"-0"+1;
+            //             alert('Next Month date is '+next_month_date);
+				        // $('#last_renewed_date').val(next_month_date);
+            //         }
+        //             else if(cur_date==30 )
+        //             {
+        //                 let next_month_date = (cur_year)+"-0"+((cur_month)+2)+"-0"+2;
+        //                 alert('Next Month date is '+next_month_date);
+				    //     $('#last_renewed_date').val(next_month_date);
+        //             }
+        //             else if(cur_date==31 )
+        //             {
+        //                 let next_month_date = (cur_year)+"-0"+((cur_month)+2)+"-0"+3;
+        //                 alert('Next Month date is '+next_month_date);
+				    //     $('#last_renewed_date').val(next_month_date);
+        //             }
+        // 			else {
+        //     			if(cur_date < 10) {
+        //     				cur_date = "0"+cur_date;
+        //     			}
+        //                 let next_month_date = (cur_year)+"-0"+((cur_month)+1)+"-"+cur_date;
+        //                 alert('Next Month date is '+next_month_date);
+    			 //       $('#last_renewed_date').val(next_month_date);
+        // 			}
+                //}  
+          //  }
             
 		}
-		
+/* -------------------------------------------------------------------  */		
 		/*if(freq==1)
 		{
 			const d = new Date(""+date);
